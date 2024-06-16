c958f7b1c6f942c7cab6299b0881abba7f4aef03
Mason Jones <20848827+smj-edison@users.noreply.github.com>
2024-06-15 18:34:00 -0700

    get lua working

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,62 @@
-use std::io::stdin;
+use std::error::Error as StdError;
+use std::fs::File;
 
-use piccolo::{meta_ops, Callback, CallbackReturn, Closure, Executor, Function, Lua};
+use rustyline::DefaultEditor;
 
-fn main() {
-    let mut lua = Lua::full();
+use piccolo::{
+    compiler::{ParseError, ParseErrorKind},
+    io, meta_ops, Callback, CallbackReturn, Closure, Executor, Function, Lua, PrototypeError,
+    StashedExecutor, StaticError,
+};
 
+fn run_code(lua: &mut Lua, executor: &StashedExecutor, code: &str) -> Result<(), StaticError> {
+    lua.try_enter(|ctx| {
+        let closure = match Closure::load(ctx, None, ("return ".to_string() + code).as_bytes()) {
+            Ok(closure) => closure,
+            Err(_) => Closure::load(ctx, None, code.as_bytes())?,
+        };
+        let function = Function::compose(
+            &ctx,
+            [
+                closure.into(),
+                Callback::from_fn(&ctx, |ctx, _, stack| {
+                    Ok(if stack.is_empty() {
+                        CallbackReturn::Return
+                    } else {
+                        CallbackReturn::Call {
+                            function: meta_ops::call(ctx, ctx.get_global("print"))?,
+                            then: None,
+                        }
+                    })
+                })
+                .into(),
+            ],
+        );
+        ctx.fetch(executor).restart(ctx, function, ());
+        Ok(())
+    })?;
+
+    lua.execute::<()>(executor)
+}
+
+fn run_repl(lua: &mut Lua) -> Result<(), Box<dyn StdError>> {
+    let mut editor = DefaultEditor::new()?;
     let executor = lua.enter(|ctx| ctx.stash(Executor::new(ctx)));
 
     loop {
+        let mut prompt = "> ";
         let mut line = String::new();
 
         loop {
-            let read = stdin().read_line(&mut line).unwrap();
-
-            let res = lua.try_enter(|ctx| {
-                let closure =
-                    Closure::load(ctx, None, ("return ".to_string() + &line).as_bytes()).unwrap();
-
-                let function = Function::compose(
-                    &ctx,
-                    [
-                        closure.into(),
-                        Callback::from_fn(&ctx, |ctx, _, stack| {
-                            Ok(if stack.is_empty() {
-                                CallbackReturn::Return
-                            } else {
-                                CallbackReturn::Call {
-                                    function: meta_ops::call(ctx, ctx.get_global("print"))?,
-                                    then: None,
-                                }
-                            })
-                        })
-                        .into(),
-                    ],
-                );
-
-                ctx.fetch(executor).restart(ctx, function, ());
-
-                Ok(())
-            });
+            let read = editor.readline(prompt)?;
+            let read_empty = read.trim().is_empty();
+            if !read_empty {
+                if !line.is_empty() {
+                    // Separate input lines in the input to the parser
+                    line.push('\n');
+                }
+                line.push_str(&read);
+            }
 
             match run_code(lua, &executor, &line) {
                 Err(StaticError::Runtime(err))
@@ -65,4 +83,12 @@ fn main() {
             }
         }
     }
+}
+
+fn main() -> Result<(), Box<dyn StdError>> {
+    let mut lua = Lua::full();
+
+    run_repl(&mut lua)?;
+
+    Ok(())
 }
